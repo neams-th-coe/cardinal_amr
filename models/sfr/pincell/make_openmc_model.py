21232ccdf37eb35be4c143c1da28b81e40adc52f
magnoxemo <walid.ahammed01@gmail.com>
2025-07-23 09:44:30 -0500

    addressing suggestions from @gonuke

--- models/sfr/pincell/make_openmc_model.py
+++ models/sfr/pincell/make_openmc_model.py
@@ -3,7 +3,7 @@ from numpy import sqrt
 from argparse import ArgumentParser
 from models.sfr import common_input as pincell_params
 import models.sfr.openmc_pincells as pins
-from models.lwr.openmc_settings import COMMON_SETTINGS
+from models.sfr.openmc_settings import COMMON_SETTINGS
 
 
 def argument_parser():
@@ -17,15 +17,16 @@ def argument_parser():
 
 def generate_pincell_model(arguments):
     pincell_universe, material = pins.PINCELLS[arguments.pincell_type]
-    fuel_bb = openmc.model.HexagonalPrism(edge_length=pincell_params.lattice_pitch / (sqrt(3)),
-                                          boundary_type="reflective", orientation="x")
-    top = openmc.ZPlane(z0=pincell_params.height, boundary_type="reflective")
-    bottom = openmc.ZPlane(z0=0, boundary_type="reflective")
 
     pincell_lattice = openmc.HexLattice()
     pincell_lattice.center = (0.0, 0.0, 0.0)
     pincell_lattice.pitch = (pincell_params.lattice_pitch, pincell_params.height / arguments.n_axial)
     pincell_lattice.universes = [[[pincell_universe]] for i in range(arguments.n_axial)]
+
+    fuel_bb = openmc.model.HexagonalPrism(edge_length=pincell_params.lattice_pitch / (sqrt(3)),
+                                          boundary_type="reflective", orientation="x")
+    top = openmc.ZPlane(z0=pincell_params.height, boundary_type="reflective")
+    bottom = openmc.ZPlane(z0=0, boundary_type="reflective")
 
     pincell_universe_base = openmc.Universe(
         cells=[openmc.Cell(fill=pincell_lattice, region=-fuel_bb & +bottom & - top)])
